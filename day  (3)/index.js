158410903d3c445f884bffb4ad82bfb688413d0a
shafikul7 <shafikul1170@gmail.com>
2025-09-12 23:14:14 +0600

    day 3

--- day  (3)/index.js
+++ day  (3)/index.js
@@ -1,5 +1,11 @@
 function addBorder(picture) {
-    
+    let wall="*".repeat(picture[0].length +2)
+    picture.unshift(wall)
+    picture.push(wall)
+    for(let i=1;i<picture.length-1;i++){
+        picture[i]="*".concat(picture[i],"*")
+    }
+    return picture;
 }
 
-console.log(addBorder())
+console.log(addBorder(['abc','ded']))
