85a46a77d43f0791f37ffbe89e38bba454b7e77c
shafikul7 <shafikul1170@gmail.com>
2025-09-24 22:32:44 +0600

    day 7

--- day  (7)/index.js
+++ day  (7)/index.js
@@ -1,5 +1,15 @@
-function addBorder(picture) {
-    
-}
+// 2160 minimum-sum-of-four-digit-number-after-splitting-digits
 
-console.log(addBorder())
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minimumSum = function (num) {
+    const arr = num.toString().split("").sort();
+    console.log(arr)
+    const sum = Number(arr[0] + arr[2]) + Number(arr[1] + arr[3]);
+    return sum;
+};
+
+console.log(minimumSum(2932))
+console.log(minimumSum(4009))
