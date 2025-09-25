f6b7e9c0711aed6856442eb5c12e52b7963bcc8b
shafikul7 <shafikul1170@gmail.com>
2025-09-25 23:09:03 +0600

    day -8

--- day  (8)/index.js
+++ day  (8)/index.js
@@ -1,1 +1,14 @@
 // palindrome-number
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let result= x.toString().split('').reverse().join('');
+    if(result == x){
+        return true;
+    }else {
+        return false;
+    }
+};
+
