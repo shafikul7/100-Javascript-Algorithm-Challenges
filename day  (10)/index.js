0a933f0731e15299e6589b4258b0584fc4e75c13
shafikul7 <shafikul1170@gmail.com>
2025-09-27 21:53:53 +0600

    day 10

---
+++ day  (10)/index.js
@@ -0,0 +1,14 @@
+// Build Array from Permutation
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function (nums) {
+    const ans = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = nums[nums[i]];
+    }
+    return ans;
+};
