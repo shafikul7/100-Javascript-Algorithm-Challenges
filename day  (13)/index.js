5844808619c20fc380c84f71ae00bf2bb21dea71
shafikul7 <shafikul1170@gmail.com>
2025-09-28 17:36:54 +0600

    day 13 and day 14

--- day  (13)/index.js
+++ day  (13)/index.js
@@ -1,1 +1,28 @@
- 
+//  1672 richest customer Wealth 
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+    let rich=[];
+    let bankCusSum=0;
+    for (let i = 0; i < accounts.length; i++) {
+        for (let b = 0; b < accounts[i].length; b++) {
+            bankCusSum +=accounts[i][b];
+            // console.log(bankCusSum);
+            
+        }
+
+        rich[i]=bankCusSum;
+        bankCusSum=0;
+    }
+    for (let r = 0; r < rich.length; r++) {
+      if(rich[0]<rich[r]){
+        rich[0]=rich[r];
+      }
+        
+    }
+    return rich[0];
+};
+
+console.log(maximumWealth([[1,2,3],[3,2,1]]))
--- day  (14)/index.js
+++ day  (14)/index.js
@@ -1,5 +1,18 @@
-function addBorder(picture) {
-    
-}
+// running-sum-of-1d-array
 
-console.log(addBorder())
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        // console.log(sum)
+        nums[i] = sum;
+        // console.log(nums)
+    }
+    return nums;
+};
+
+console.log(runningSum([1,2,3,4,5,6,7,8,9,10]));
