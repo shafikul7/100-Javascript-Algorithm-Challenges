427b60ddd78a040387a423b7ab0c504c4b607166
shafikul7 <shafikul1170@gmail.com>
2025-09-28 21:30:15 +0600

    day 16

--- day  (15)/index.js
+++ day  (15)/index.js
@@ -1,5 +1,21 @@
-function addBorder(picture) {
-    
-}
+// how-many-numbers-are-smaller-than-the-current-number
 
-console.log(addBorder())
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    let ansAry=[];
+    let count =0;
+    for(let i=0;i<nums.length;i++){
+        for(let j=0 ;j<nums.length;j++){
+            if(nums[j]<nums[i] && j!=i){
+                count += 1;
+            }
+        }
+        ansAry[i]=count;
+        count =0;
+        
+    }
+    return ansAry
+};
--- day  (16)/index.js
+++ day  (16)/index.js
@@ -1,5 +1,19 @@
-function addBorder(picture) {
-    
-}
+// defanging-an-ip-address
 
-console.log(addBorder())
+/**
+ * @param {string} address
+ * @return {string}
+ */
+var defangIPaddr = function(address) {
+    
+   const str=address.split('');
+   for (let index = 0; index < str.length; index++) {
+    if(str[index]== '.'){
+        str[index]='[.]';
+    }
+    
+   }
+   return str.join('');
+};
+
+console.log(defangIPaddr('1.1.1.1'))
